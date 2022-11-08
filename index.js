d35f95079ba17e9c70f8748ba36d0df7fd8ae470
shahzad761 <shahzadrafiq1111@gmail.com>
2022-11-08 13:00:11 +0500

    2nd commit

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const { response } = require('express')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 const pool = require('./dbcon')
-
+app.use(bodyParser.json())
 
 app.get('/', async (req, response) => {
   
     let ress = await pool.query('select * from public.todoList')
     console.log(ress)
     
-    response.json({info:'Node.js,Express,Express and Postgres API'})
+    response.json({info: ress.rows})
+})
+
+
+app.post('/p', async (req, response) => {
+  console.log(req.body)
+  console.log(req.body.id)
+  let ressult = await pool.query('insert into public.todoList (id,task,done) values ($1,$2,$3)',[req.body.id, req.body.task, req.body.done])
+  console.log (ressult)
+  
+  response.json({ "info":"Task Created" })
+})
+
+
+app.put('/update', async (req, response) => {
+  console.log(req.body)
+  console.log(req.body.id)
+  let ressult = await pool.query('update public.todoList set task=$1 where id = $2',[req.body.task, req.body.id])
+  console.log (ressult)
+  
+  response.json({ "info":"Task Created" })
 })
 
 app.listen(port, () => {
